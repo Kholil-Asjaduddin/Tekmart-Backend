2a64bbb56023e485bfd7871894a6996bf65cbc86
danellazs <danella.zefanya@gmail.com>
2024-09-23 12:43:38 +0700

    Blacklist token for logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Blacklist = require('../models/Blacklist');
 const bcrypt = require('bcryptjs');// npm install express bcryptjs jsonwebtoken
 const jwt = require('jsonwebtoken'); 
 
@@ -30,6 +31,7 @@ const registerUser = async (req, res) => {
           name: newUser.name,
           email: newUser.email,
           isAdmin: newUser.isAdmin,
+          token: generateToken(newUser),
         });
       })
   } catch (error) {
@@ -62,8 +64,26 @@ const loginUser = async (req, res) => {
 };
 
 // Logout User
-const logoutUser = (req, res) => {
-  res.status(200).json({ message: 'User logged out' });
+const logoutUser = async (req, res) => {
+  try {
+    const authHeader = req.headers['authorization']; // Get the token from the authorization header
+    if (!authHeader) return res.sendStatus(204); // No token provided, just return success
+
+    const token = authHeader.split(' ')[1]; // Extract the token from the Bearer scheme
+    const checkIfBlacklisted = await Blacklist.findOne({ token }); // Check if token is already blacklisted
+
+    if (checkIfBlacklisted) {
+      return res.sendStatus(204); // If already blacklisted, no need to blacklist again
+    }
+
+    // Add token to blacklist
+    const newBlacklist = new Blacklist({ token });
+    await newBlacklist.save();
+
+    res.status(200).json({ message: 'User successfully logged out' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
 };
 
 const generateToken = (user) => {
@@ -72,8 +92,29 @@ const generateToken = (user) => {
   });
 };
 
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers['authorization']; // Extract token from Authorization header
+  if (!authHeader) return res.status(401).json({ message: 'No token provided' });
+
+  const token = authHeader.split(' ')[1]; // Extract token from Bearer scheme
+
+  // Check if token is blacklisted
+  const blacklistedToken = await Blacklist.findOne({ token });
+  if (blacklistedToken) {
+    return res.status(401).json({ message: 'Token has been blacklisted. Please log in again.' });
+  }
+
+  // If not blacklisted, verify the token
+  jwt.verify(token, 'secretkey', (err, decoded) => {
+    if (err) return res.status(403).json({ message: 'Token is invalid' });
+    req.user = decoded; // Attach the user info to the request object
+    next(); // Continue to the next middleware or route handler
+  });
+};
+
 module.exports = {
   registerUser,
   loginUser,
-  logoutUser
+  logoutUser,
+  verifyToken
 };
