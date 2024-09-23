0fc28bde1b3cc99408e6f15d287c8b2e62b27dad
eben4ya <benaya.imanuela@gmail.com>
2024-09-23 21:37:07 +0700

    <feat: added payment schema>

---
+++ models/Payment.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Order',
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: 'Pending',
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Payment', paymentSchema);
