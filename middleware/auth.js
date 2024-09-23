2bfc0857dc9e854b0077317634a2cf61eb4a2f48
eben4ya <benaya.imanuela@gmail.com>
2024-09-23 21:47:53 +0700

    <feat: added middleware to check if user is logged in or not>

--- middleware/auth.js
+++ middleware/auth.js
@@ -25,4 +25,27 @@ exports.isAdmin = async (req, res, next) => {
       res.status(500).json({ message: 'Failed to authenticate token', error: err });
     }
   };
+
+  // Middleware for checking if user is logged in
+  exports.protect = async (req, res, next) => {
+    let token;
+  
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      try {
+        token = req.headers.authorization.split(' ')[1];
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select('-password');
+        next();
+      } catch (error) {
+        res.status(401).json({ message: 'Not authorized, token failed' });
+      }
+    }
+  
+    if (!token) {
+      res.status(401).json({ message: 'Not authorized, no token' });
+    }
+  };
   
