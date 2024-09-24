4ff87f74f59c0a401b8376ba79c707b6c55c6f4c
eben4ya <benaya.imanuela@gmail.com>
2024-09-24 12:38:23 +0700

    <fix: isAdmin auth>

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,30 +1,44 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User'); // Model pengguna
 
-// Untuk ngecek admin pake middleware 
+// Middleware for checking if user is an admin
 exports.isAdmin = async (req, res, next) => {
-    const token = req.headers['authorization'];
-  
-    if (!token) {
-      return res.status(403).json({ message: 'No token provided' });
-    }
-  
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
     try {
+      // get token from header
+      token = req.headers.authorization.split(' ')[1];
+      
+      // veryfy jwt token
       const decoded = jwt.verify(token, 'secretkey');
-      const user = await User.find({ id: decoded.id }); // Gunakan async/await di sini
+      
+      // find user by id from decoded token
+      const user = await User.findById(decoded.id);
+
+      // if user not found
       if (!user) {
         return res.status(404).json({ message: 'User not found!' });
       }
-      if (this.isAdmin == false) {
+
+      // checking if user is admin
+      if (!user.isAdmin) {
         return res.status(403).json({ message: 'Require Admin Role!' });
       }
-  
+
+      // if user is admin, continue to next middleware
       next();
     } catch (err) {
-      console.error('Error during token verification or user lookup:', err); // Tambahkan log kesalahan
-      res.status(500).json({ message: 'Failed to authenticate token', error: err });
+      console.error('Error during token verification or user lookup:', err); 
+      return res.status(500).json({ message: 'Failed to authenticate token', error: err.message });
     }
-  };
+  } else {
+    return res.status(403).json({ message: 'No token provided' });
+  }
+};
 
   // Middleware for checking if user is logged in
   exports.protect = async (req, res, next) => {
