350c492eb345287cace173a46fbfb925ce9b0ee9
eben4ya <benaya.imanuela@gmail.com>
2024-11-13 18:30:06 +0700

    <feat: added function to send transaction token & redirect url to frontend>

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,33 +1,50 @@
-const Payment = require('../models/Payment');
-const Order = require('../models/Order');
+const Payment = require("../models/Payment");
+const Order = require("../models/Order");
+const snap = require("../configs/midtrans");
 
 // Process Payment
 exports.processPayment = async (req, res) => {
-  const { orderId, totalPrice, paymentMethod } = req.body;
+  const { orderId, totalPrice, customerDetails } = req.body;
 
   try {
     const order = await Order.findById(orderId);
 
     if (!order) {
-      return res.status(404).json({ message: 'Order not found' });
+      return res.status(404).json({ message: "Order not found" });
     }
 
-    // Simulate payment processing
+    // Create transaction parameters for Midtrans
+    const transactionParams = {
+      transaction_details: {
+        order_id: orderId,
+        gross_amount: totalPrice,
+      },
+      customer_details: customerDetails,
+    };
+
+    // Create transaction using Midtrans Snap
+    const transaction = await snap.createTransaction(transactionParams);
+    const transactionToken = transaction.token;
+    const redirectUrl = transaction.redirect_url;
+
+    // Create payment object with pending status
     const payment = new Payment({
       orderId,
       totalPrice,
-      paymentMethod,
-      status: 'Completed',
-      transactionId: 'TXN' + Date.now(),
+      paymentMethod: "Midtrans",
+      status: "Pending",
+      transactionId: transactionToken,
     });
 
-    const savedPayment = await payment.save();
+    // Save payment object to database
+    await payment.save();
 
-    // Update Order status
-    order.status = 'Paid';
-    await order.save();
-
-    res.status(201).json(savedPayment);
+    // Uncomment this code to update order status to "Paid" after payment
+    //  order.status = "Paid";
+    //  await order.save();
+ 
+    // Respond with the transaction token for frontend to proceed with payment
+    res.status(201).json({ token: transactionToken, redirect_url: redirectUrl });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -41,7 +58,7 @@ exports.getPaymentByOrderId = async (req, res) => {
     if (payment) {
       res.json(payment);
     } else {
-      res.status(404).json({ message: 'Payment not found' });
+      res.status(404).json({ message: "Payment not found" });
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
