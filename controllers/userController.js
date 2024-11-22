efdabe565331ddda7ea9394f7e9c5cec905bd342
eben4ya <benaya.imanuela@gmail.com>
2024-11-22 15:18:59 +0700

    feat: saved user token using cookie

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,33 +1,35 @@
-const User = require('../models/User');
-const Blacklist = require('../models/Blacklist');
-const bcrypt = require('bcrypt');// npm install express bcryptjs jsonwebtoken
-const jwt = require('jsonwebtoken'); 
+const User = require("../models/User");
+const Blacklist = require("../models/Blacklist");
+const bcrypt = require("bcrypt"); // npm install express bcryptjs jsonwebtoken
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
   try {
-      const { email, password } = req.body;
-      if (!email || !password) {
-        return res.status(400).json({ message: 'Please provide all required fields (name, email, password)' });
-      }
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Please provide all required fields (name, email, password)",
+      });
+    }
 
-      const saltRounds = 12;
-      const hashedPassword = await bcrypt.hash(password, saltRounds); 
-      const newUser = new User({ email, password: hashedPassword }); 
-      await newUser.save();
-      res.status(201).json({
-          statusCode: 201,
-          message: 'User added successfully',
-          data: newUser    
-      });
+    const saltRounds = 12;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const newUser = new User({ email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({
+      statusCode: 201,
+      message: "User added successfully",
+      data: newUser,
+    });
   } catch (error) {
-      res.status(500).json({
-          statusCode: 500,
-          message: 'Failed to add new user', 
-          error: error.message
-      });
+    res.status(500).json({
+      statusCode: 500,
+      message: "Failed to add new user",
+      error: error.message,
+    });
   }
 };
-  
+
 // Login User
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
@@ -35,32 +37,39 @@ const loginUser = async (req, res) => {
   try {
     const user = await User.findOne({ email }); // Gunakan findOne dan await
 
+    const token = generateToken(user);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.EXPRESS_ENV === "production",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+
     if (user && (await bcrypt.compare(password, user.password))) {
       res.json({
         id: user.id,
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
-        token: generateToken(user),
       });
     } else {
-      res.status(401).json({ message: 'Email atau kata sandi tidak valid' });
+      res.status(401).json({ message: "Email atau kata sandi tidak valid" });
     }
   } catch (error) {
-    console.error('Error during login:', error); // Tambahkan log kesalahan
-    res.status(500).json({ message: 'Kesalahan server', error });
+    console.error("Error during login:", error); // Tambahkan log kesalahan
+    res.status(500).json({ message: "Kesalahan server", error });
   }
 };
 
 // Logout User
 const logoutUser = async (req, res) => {
   try {
-    const authHeader = req.headers['authorization']; // Get the token from the authorization header
-    console.log(authHeader)
+    const authHeader = req.headers["authorization"]; // Get the token from the authorization header
+    console.log(authHeader);
     if (!authHeader) return res.sendStatus(204); // No token provided, just return success
 
-    const token = authHeader.split(' ')[1]; // Extract the token from the Bearer scheme
-    const tokenHash = crypto.createHash('sha256').update(token).digest('hex'); // Hash token before storing in the database
+    const token = authHeader.split(" ")[1]; // Extract the token from the Bearer scheme
+    const tokenHash = crypto.createHash("sha256").update(token).digest("hex"); // Hash token before storing in the database
     const checkIfBlacklisted = await Blacklist.findOne({ token: tokenHash }); // Check if token is already blacklisted
 
     if (checkIfBlacklisted) {
@@ -71,34 +80,40 @@ const logoutUser = async (req, res) => {
     const newBlacklist = new Blacklist({ token: tokenHash });
     await newBlacklist.save();
 
-    res.status(200).json({ message: 'User successfully logged out' });
+    res.clearCookie("token"); // Clear the httpOnly cookie
+    res.status(200).json({ message: "User successfully logged out" });
   } catch (error) {
-    res.status(500).json({ message: 'Server error', error });
+    res.status(500).json({ message: "Server error", error });
   }
 };
 
 const generateToken = (user) => {
-  return jwt.sign({ id: user.id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  });
+  return jwt.sign(
+    { id: user.id, isAdmin: user.isAdmin },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
 };
 
 const verifyToken = async (req, res, next) => {
-  const authHeader = req.headers['authorization']; // Extract token from Authorization header
-  if (!authHeader) return res.status(401).json({ message: 'No token provided' });
+  const token = req.cookies.token; // Extract token from httpOnly cookie
+  if (!token) return res.status(401).json({ message: "No token provided" });
 
-  const token = authHeader.split(' ')[1]; // Extract token from Bearer scheme
-  const tokenHash = crypto.createHash('sha256').update(token).digest('hex'); // Hash token before checking in the database
+  const tokenHash = crypto.createHash("sha256").update(token).digest("hex"); // Hash token before checking in the database
 
   // Check if token is blacklisted
   const blacklistedToken = await Blacklist.findOne({ token: tokenHash });
   if (blacklistedToken) {
-    return res.status(401).json({ message: 'Token has been blacklisted. Please log in again.' });
+    return res
+      .status(401)
+      .json({ message: "Token has been blacklisted. Please log in again." });
   }
 
   // If not blacklisted, verify the token
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ message: 'Token is invalid' });
+    if (err) return res.status(403).json({ message: "Token is invalid" });
     req.user = decoded; // Attach the user info to the request object
     next(); // Continue to the next middleware or route handler
   });
@@ -108,5 +123,5 @@ module.exports = {
   registerUser,
   loginUser,
   logoutUser,
-  verifyToken
+  verifyToken,
 };
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,65 +1,56 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User'); // Model pengguna
+const jwt = require("jsonwebtoken");
+const User = require("../models/User"); // Model pengguna
 
 // Middleware for checking if user is an admin
 exports.isAdmin = async (req, res, next) => {
-  let token;
+  let token = req.cookies.token; // get token from cookie
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
+  if (token) {
     try {
-      // get token from header
-      token = req.headers.authorization.split(' ')[1];
-      
       // veryfy jwt token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      
+
       // find user by id from decoded token
       const user = await User.findById(decoded.id);
 
       // if user not found
       if (!user) {
-        return res.status(404).json({ message: 'User not found!' });
+        return res.status(404).json({ message: "User not found!" });
       }
 
       // checking if user is admin
       if (!user.isAdmin) {
-        return res.status(403).json({ message: 'Require Admin Role!' });
+        return res.status(403).json({ message: "Require Admin Role!" });
       }
 
       // if user is admin, continue to next middleware
       next();
     } catch (err) {
-      console.error('Error during token verification or user lookup:', err); 
-      return res.status(500).json({ message: 'Failed to authenticate token', error: err.message });
+      console.error("Error during token verification or user lookup:", err);
+      return res
+        .status(500)
+        .json({ message: "Failed to authenticate token", error: err.message });
     }
   } else {
-    return res.status(403).json({ message: 'No token provided' });
+    return res.status(403).json({ message: "No token provided" });
   }
 };
 
-  // Middleware for checking if user is logged in
-  exports.protect = async (req, res, next) => {
-    let token;
-  
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith('Bearer')
-    ) {
-      try {
-        token = req.headers.authorization.split(' ')[1];
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.id).select('-password');
-        next();
-      } catch (error) {
-        res.status(401).json({ message: 'Not authorized, token failed' });
-      }
+// Middleware for checking if user is logged in
+exports.protect = async (req, res, next) => {
+  let token = req.cookies.token; // get token from cookie
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Not authorized, token failed" });
     }
-  
-    if (!token) {
-      res.status(401).json({ message: 'Not authorized, no token' });
-    }
-  };
-  
+  }
+
+  if (!token) {
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
