cac318463068a6561b56cca50a413942a4b4cf60
eben4ya <benaya.imanuela@gmail.com>
2024-12-22 18:34:55 +0700

    feat: added delete product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const productController = require('../controllers/productController');
-const {isAdmin} = require('../middleware/auth');
+const express = require("express");
+const productController = require("../controllers/productController");
+const { isAdmin } = require("../middleware/auth");
 const router = express.Router();
 
 // Route menambahkan produk baru (hanya untuk role admin)
-router.post('/', isAdmin, productController.addProduct);
+router.post("/", isAdmin, productController.addProduct);
 // Route mengubah informasi produk (hanya untuk role admin)
-router.put('/:id', isAdmin, productController.updateProduct);
+router.put("/:id", isAdmin, productController.updateProduct);
+// Route menghapus produk (hanya untuk role admin)
+router.delete("/:id", isAdmin, productController.deleteProductById);
 // Route mendapatkan informasi seluruh produk
-router.get('/', productController.getAllProduct);
+router.get("/", productController.getAllProduct);
 // Route mendapatkan informasi produk dengan id tertentu
-router.get('/:id', productController.getProductById);
+router.get("/:id", productController.getProductById);
 // Route mendapatkan informasi produk dengan kategori tertentu
-router.get('/category/:category', productController.getProductByCategory);
+router.get("/category/:category", productController.getProductByCategory);
 
 module.exports = router;
